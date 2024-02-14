a291d68ad3d7d89389d1b060f59b018e12b17454
Debroucker Baptiste <156613178+Earoldor@users.noreply.github.com>
2024-02-14 13:01:41 +0100

    Add files via upload

---
+++ prepaTPbiblio/biblio.c
@@ -0,0 +1,20 @@
+#include "biblio.h"
+
+void init (T_Bibliotheque *ptrB)
+{
+    ptrB->nbLivres=0;
+}
+
+
+int ajouterLivre(T_Bibliotheque  *ptrB)
+{
+    
+    return 0;
+}
+
+
+int  afficherBibliotheque(const T_Bibliotheque  *ptrB)
+{
+    
+    return 0;
+}
